c3c44ff5e85a010c9ba8f4148451435128d20e69
Billy Li <li000400@umn.edu>
2021-12-01 22:34:02 -0600

    debug

--- python/Utility/Architectures.py
+++ python/Utility/Architectures.py
@@ -121,46 +121,46 @@ class VGG16_bottleNeck:
         x = Conv2D(36, (3, 3), activation='relu', padding='same', name='block1_conv1', trainable=trainable)(input_layer)
         x = Conv2D(36, (3, 3), activation='relu', padding='same', name='block1_conv2', trainable=trainable)(x)
 
-        x = Conv2D(18, (3, 3), activation='relu', padding='same', name='block1_conv1', trainable=trainable)(x)
-        x = Conv2D(18, (3, 3), activation='relu', padding='same', name='block1_conv2', trainable=trainable)(x)
+        x = Conv2D(18, (3, 3), activation='relu', padding='same', name='block2_conv1', trainable=trainable)(x)
+        x = Conv2D(18, (3, 3), activation='relu', padding='same', name='block2_conv2', trainable=trainable)(x)
 
-        x = Conv2D(3, (3, 3), activation='relu', padding='same', name='block1_conv1', trainable=trainable)(x)
-        x = Conv2D(3, (3, 3), activation='relu', padding='same', name='block1_conv2', trainable=trainable)(x)
+        x = Conv2D(3, (3, 3), activation='relu', padding='same', name='block3_conv1', trainable=trainable)(x)
+        x = Conv2D(3, (3, 3), activation='relu', padding='same', name='block3_conv2', trainable=trainable)(x)
 
         # Block 2
-        x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block2_conv1', trainable=trainable)(x)
-        x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block2_conv2', trainable=trainable)(x)
+        x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block4_conv1', trainable=trainable)(x)
+        x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block4_conv2', trainable=trainable)(x)
         x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
 
         # Block 3
-        x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block3_conv1', trainable=trainable)(x)
-        x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block3_conv2', trainable=trainable)(x)
-        x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block3_conv3', trainable=trainable)(x)
+        x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block5_conv1', trainable=trainable)(x)
+        x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block5_conv2', trainable=trainable)(x)
+        x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block5_conv3', trainable=trainable)(x)
         x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
 
         # Block 4
-        x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block4_conv1', trainable=trainable)(x)
+        x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block6_conv1', trainable=trainable)(x)
         #x = SpatialDropout2D(0.3)(x)
-        x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block4_conv2', trainable=trainable)(x)
+        x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block6_conv2', trainable=trainable)(x)
         #x = SpatialDropout2D(0.3)(x)
-        x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block4_conv3', trainable=trainable)(x)
+        x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block6_conv3', trainable=trainable)(x)
         #x = SpatialDropout2D(0.3)(x)
         x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
 
 
         # Block 5
-        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1', trainable=trainable)(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block7_conv1', trainable=trainable)(x)
         #x = SpatialDropout2D(0.3)(x)
-        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2', trainable=trainable)(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block7_conv2', trainable=trainable)(x)
         #x = SpatialDropout2D(0.3)(x)
-        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3', trainable=trainable)(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block7_conv3', trainable=trainable)(x)
         x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
 
-        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1', trainable=trainable)(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block8_conv1', trainable=trainable)(x)
         #x = SpatialDropout2D(0.3)(x)
-        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2', trainable=trainable)(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block8_conv2', trainable=trainable)(x)
         #x = SpatialDropout2D(0.3)(x)
-        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3', trainable=trainable)(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block8_conv3', trainable=trainable)(x)
 
         return x
 
