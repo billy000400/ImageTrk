5fd8496a6fec82136375c53c90565d27741d4763
Billy Li <li000400@umn.edu>
2022-01-04 15:17:42 -0600

    dev

--- python/wcnn/wcnn_train.py
+++ python/wcnn/wcnn_train.py
@@ -24,9 +24,22 @@ from Metric import *
 
 def train(C):
 
+    # prepare data generator
     train_generator = DataGeneratorV2(C.X_train_dir, C.Y1_train_dir, C.Y2_train_dir, batch_size=1)
     val_generator = DataGeneratorV2(C.X_val_dir, C.Y1_val_dir, C.Y2_val_dir, batch_size=1)
 
+    # outputs
+    data_dir = C.sub_data_dir
+    weight_dir = C.data_dir.parent.joinpath('weights')
+    weight_dir.mkdir(parents=True, exist_ok=True)
+    C.weight_dir = weight_dir
+
+    model_weights_file = weight_dir.joinpath(C.rpn_model_name+'.h5')
+    record_file = data_dir.joinpath(C.rpn_record_name+'.csv')
+
+    pinfo('I/O Path is configured')
+
+    # build the model
     model = Img2Vec(input_shape=(256, 256, 1)).get_model()
     model.summary()
 
@@ -39,16 +52,32 @@ def train(C):
         decay_rate=0.9)
     adam = Adam(learning_rate=lr_schedule)
 
+    # setup callbacks
+    CsvCallback = tf.keras.callbacks.CSVLogger(str(record_file), separator=",", append=False)
+    ModelCallback = tf.keras.callbacks.ModelCheckpoint(model_weights_file,\
+                        monitor='val_loss', verbose=1,\
+                        save_weights_only=True,\
+                        save_best_only=True, mode='auto', save_freq='epoch')
+
+    logdir="logs/fit/" + "wcnn_" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+
+    # compile the model
     model.compile(optimizer=adam, loss={'classifier': classifier_loss,\
                                         'regressor': regressor_loss},
                                     metrics={'classifier': [unmasked_binary_accuracy, positive_number],\
-                                            'regressor': None})
+                                            'regressor': unmasked_IoU1D})
 
+    # initialize fit parameters
     model.fit(x=train_generator,
                 validation_data=val_generator,\
                 shuffle=True,\
-                epochs=1000)
-    return
+                callbacks = [CsvCallback, ModelCallback, tensorboard_callback],\
+                epochs=2000)
+
+    pinfo(f"Weights are saved to {str(model_weights_file)}")
+    pcheck_point('Finished Training')
+    return C
 
 if __name__ == "__main__":
     pbanner()
@@ -56,8 +85,15 @@ if __name__ == "__main__":
     pmode('Generating validating data')
     pinfo('Input DType for testing: StrawHit')
 
+    # load configuration
     cwd = Path.cwd()
     pickle_path = cwd.joinpath('wcnn.config.pickle')
     C = pickle.load(open(pickle_path,'rb'))
 
-    train(C)
+    # initialize parameters
+    model_name = 'wcnn_00'
+    record_name = 'wcnn_record_00'
+    C.set_outputs(model_name, record_name)
+
+    C = train(C)
+    pickle.dump(C, open(pickle_path, 'wb'))
