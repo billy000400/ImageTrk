ea812008f027499e86fc3b8047d70f469ec54bb6
Billy Li <li000400@umn.edu>
2022-01-12 13:10:49 -0600

    debug

--- python/evaluation/window_proposal/make_data.py
+++ python/evaluation/window_proposal/make_data.py
@@ -184,7 +184,7 @@ if __name__ == "__main__":
 
     dp_list = ['train_CeEndpoint-mix']
     resolution = 256
-    eventNum = 560
+    eventNum = 723
 
     C.set_train_dp_list(dp_list)
     C.set_resolution(resolution)
